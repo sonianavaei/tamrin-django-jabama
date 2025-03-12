86b9ebbda9ce1126f0ac576e26df9c375fb7358d
sonianavaei <sonianavaei@gmail.com>
2025-03-12 18:36:29 +0330

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApartmentConfig(AppConfig):
+class CottageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apartment'
+    name = 'cottage'
--- urls.py
+++ urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello, name='apartment_hello'),
+    path('hello/', views.hello, name='cottage_hello'),
 ]
-
